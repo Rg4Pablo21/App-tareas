91df8be16bf9507bdf92f3590f5b5eed8df090db
fox <gic438@gmail.com>
2025-02-04 17:17:04 -0600

    Se termina el logo Se trabaja en los siguientes archivos -header.js -header.css

--- componentes/header/header.js
+++ componentes/header/header.js
@@ -1,8 +1,18 @@
-function header(){ 
+function header(){
+    let header = document.createElement('header');
+    header.className = "header";
 
-let header = document.createElement("header")
-header.className = "header";
-return header;
+    let logo = document.createElement('img');
+    logo.className="imagen"
+    logo.src = "https://isenacode.com/wp-content/uploads/2021/12/escribir-logo-apple-d.jpg";
+    logo.alt = "";
+    header.appendChild(logo);
+
+    let h1 = document.createElement('h1');
+    h1.innerText = "Let´s go";
+    header.appendChild(h1);
+
+    return header;
 }
 
 export {header}
