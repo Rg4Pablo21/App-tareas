38a9bcae93b759de090632b6b8a7f6e41796abc2
fox <gic438@gmail.com>
2025-02-14 11:41:48 -0600

    Se termina la checkbox

--- componentes/contenedor/contenedor.js
+++ componentes/contenedor/contenedor.js
@@ -1,29 +1,44 @@
-function contenedor() { 
-    let contenedor = document.createElement("div");
-    contenedor.className = "contenedor";
+    function contenedor() { 
+        let contenedor = document.createElement("div");
+        contenedor.className = "contenedor";
 
-    for (let i = 1; i <= 12; i++) {
-        let div = document.createElement("div");
-        div.className = `cuadro-${i}`;
+        let header = document.createElement("h2");
+        header.innerText = "Personal";
+        header.style.marginBottom = "20px"; 
 
-        // Crear checkbox
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.className = "checkbox";
+        for (let i = 1; i <= 12; i++) {
+            let div = document.createElement("div");
+            div.className = `cuadro cuadro-${i}`;
 
-        // Etiqueta con texto
-        let label = document.createElement("label");
-        label.innerText = `Cuadro ${i}`;
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.className = "checkbox";
 
-        div.appendChild(checkbox);
-        div.appendChild(label);
-        contenedor.appendChild(div);
+            let label = document.createElement("label");
+            label.innerText = `Cuadro ${i}`;
+            label.className = "texto-cuadro";
 
+            let cuadroDiv = document.createElement("div");
+            cuadroDiv.className = `div-cuadro-${i}`;
 
+            
+
+            div.addEventListener("click", function () {
+                checkbox.checked = !checkbox.checked;
+                div.classList.toggle("seleccionado", checkbox.checked);
+                label.classList.toggle("texto-seleccionado", checkbox.checked);
+            });
         
+
+
+
+            div.appendChild(checkbox);
+            div.appendChild(label);
+            div.appendChild(cuadroDiv);
+            contenedor.appendChild(div);
+        }
+
+        return contenedor;
     }
 
-    return contenedor;
-}
-
-export { contenedor };
+    export { contenedor };
