aee868bd262ff97d511f81f6dd1832f89b68d17e
fox <gic438@gmail.com>
2025-03-06 21:39:34 -0600

    Terminado el arreglo

--- componentes/header/header.js
+++ componentes/header/header.js
@@ -4,12 +4,13 @@ function header(){
 
     let logo = document.createElement('img');
     logo.className="imagen"
-    logo.src = "https://static-cse.canva.com/blob/951768/1650logotiposqueteinspiraran.png";
+    logo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjwwJkOTCniBk3XcgBMR6hzexhFt530sIHlg&s";
     logo.alt = "";
     header.appendChild(logo);
 
     let h1 = document.createElement('h1');
-    h1.innerText = "Let´s go";
+    h1.innerText = "Personal";
+    h1.className="texto"
     header.appendChild(h1);
 
     return header;
--- main.js
+++ main.js
@@ -2,13 +2,11 @@ import { footer } from "./componentes/footer/footer.js";
 import { header } from "./componentes/header/header.js";
 import { contenedor } from "./componentes/contenedor/contenedor.js";
 
-let DOM = document.querySelector("#root")
 
-DOM.appendChild(footer());
+let DOM = document.querySelector("#root");
+
+
+
 DOM.appendChild(header());
 DOM.appendChild(contenedor());
-
-
-
-
-
+DOM.appendChild(footer());
